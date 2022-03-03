8a80f69dd4813c84feeaf34a57d13e1b835ab944
Roland-Sankara <roland1sankara@gmail.com>
2022-03-03 23:30:16 +0300

    Feature: Creats a new task - add function to add new task - disable forward and backward arrows appropriately

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -6,14 +6,39 @@ export default class KanbanBoard extends Component {
     super();
     // Each task is uniquely identified by its name. 
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
+    // { name: '1', stage: 0 },
+    // { name: '2', stage: 0 },
     this.state = {
-      tasks: [
-            { name: '1', stage: 0 },
-            { name: '2', stage: 0 },
-        ]
+      tasks: [],
+      newTask:''
     };
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.onchange = this.onchange.bind(this);
+    this.addNewTask = this.addNewTask.bind(this);
   }
+
+  // Function to collect the new task from the input onchange
+  onchange(e){
+    let value = e.target.value;
+    this.setState({newTask:value})
+  }
+  // Function to add new task to tasks state array
+  addNewTask(){
+    if(this.state.newTask){
+      this.setState({tasks:[...this.state.tasks,{name:this.state.newTask,stage:0}]});
+      this.setState({newTask:''});
+    }
+  }
+  
+  // Function to elevate the task stage
+  elevateStage(){
+    
+  }
+  // Function to lower the task stage
+
+
+
+
 
   render() {
     const { tasks } = this.state;
@@ -30,8 +55,8 @@ export default class KanbanBoard extends Component {
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <section className="mt-50 layout-row align-items-center justify-content-center">
-          <input id="create-task-input" type="text" className="large" placeholder="New task name" data-testid="create-task-input"/>
-          <button type="submit" className="ml-30" data-testid="create-task-button">Create task</button>
+          <input value={this.state.newTask} onChange={(e)=>{this.onchange(e)}} id="create-task-input" type="text" className="large" placeholder="New task name" data-testid="create-task-input"/>
+          <button onClick={this.addNewTask} type="submit" className="ml-30" data-testid="create-task-button">Create task</button>
         </section>
 
         <div className="mt-50 layout-row">
@@ -46,10 +71,10 @@ export default class KanbanBoard extends Component {
                                       <div className="li-content layout-row justify-content-between align-items-center">
                                         <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
                                         <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
+                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`} disabled={task.stage === 0?true:false}>
                                             <i className="material-icons">arrow_back</i>
                                           </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
+                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`} disabled={task.stage === 3?true:false}>
                                             <i className="material-icons">arrow_forward</i>
                                           </button>
                                           <button className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`}>
